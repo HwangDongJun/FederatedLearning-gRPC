13448102df0089680066acd4a84ca972994767d2
DongjunHwang <enter031@naver.com>
2021-08-10 13:56:42 +0900

    feat: get initial model parameter from server and update local model with own data

--- client_fit_model.py
+++ client_fit_model.py
@@ -1,14 +1,19 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
+import pickle
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
+from datetime import datetime
 
 
 class learning_fit(object):
-	def __init__(self):
-		self.temp = ""
+	def __init__(self, mt, ec, pr, cr):
+		self.model_type = mt
+		self.epochs = ec
+		self.params = pickle.loads(pr)
+		self.round = cr
 
 	def image_generator(self):
 		image_gen_train = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255,
@@ -21,7 +26,7 @@ class learning_fit(object):
 	def gen_train_val_data(self, bs=16, ims=(224, 224), cn=np.array(['0', '1', '2', '3', '4'])):
 		gen_train, gen_val = self.image_generator()
 
-		train_data_dir = os.path.abspath('/home/dnlab/federated_grpc/data_balance/train/')
+		train_data_dir = os.path.abspath('/home/dnlab/Downloads/data_balance/train/')
 		train_data_gen = gen_train.flow_from_directory(directory=str(train_data_dir),
 														batch_size=bs,
 														color_map='rgb',
@@ -30,9 +35,52 @@ class learning_fit(object):
 														classes=list(cn))
 		return train_data_gen
 
-	def manage_train(self, cr, params): # cr:current_round
-		get_weights = list()
-		if cr != 1: # 첫 학습
+	def change_model_layers(self, model):
+		base_input = model.layers[0].input
+		base_output = model.layers[-2].output
+		
+		# 지금은 아니지만 서버와 클라이언트가 학습 모델에 대한 정보를 알 수 있는 방법도 코드 구현해야함
+		final_output = layers.Dense(128)(base_output)
+		final_output = layers.Activation('relu')(final_output)
+		final_output = layers.Dense(64)(final_output)
+		final_output = layers.Activation('relu')(final_output)
+		final_output = layers.Dense(5, activation='softmax')(final_output)
+
+		return keras.Model(inputs=base_input, outputs=final_output)
+
+	def build_model(self):
+		if self.model_type == "mobilenet_v2":
+			model = tf.keras.applications.MobileNetV2()
+			new_model = self.change_model_layers(model)
+			new_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 			
-		else:
+		return new_model
+
+	def train_model_tosave(self):
+		earlystopping_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+		logdir = f"logs/{datetime.now().strftime(\"%Y%m%d-%H%M%S\")}-{self.round}"
+		tensorboard_callback = tf.keras.callbacks.TensorBoard(
+														log_dir=logdir,
+														histogram_freq=1,
+														write_graph=True,
+														update_freq='epoch',
+														profile_batch=2,
+														embeddings_freq=1)
+
+		local_model = self.build_model()
+		local_mode.set_weights(self.params)
+
+		gen_train_data = self.get_train_val_data()
+		local_model.fit_generator(gen_train_data, epochs=self.epochs, callbacks=[callback, tensorboard_callback])
+
+		# Export model
+		#export_path = "?"
+		#local_model.save(export_path, save_format="tf")
 			
+		return local_model
+
+	def manage_train(self): # cr:current_round
+		if self.params == list():
+			return []
+		lmodel = self.train_model_tosave()
+		return lmodel.get_weights()
--- fl_client.py
+++ fl_client.py
@@ -1,40 +1,53 @@
 from __future__ import print_function
 
+import random
 import grpc
 from transport_pb2 import Scalar, transportRequest, ReadyReq, UpdateReq, State
 from transport_pb2_grpc import TransportServiceStub
 
+from client_fit_model import learning_fit
 
-def request_update():
+def request_parameter():
 	update_request = [UpdateReq(type="P")]
 	for ur in update_request:
 		yield transportRequest(update_req=ur)
 
 def request_ready():
+	global client_name
+
 	configuration = dict()
 	configuration['current_round'] = Scalar(scint32=0)
 
-	ready_request = [ReadyReq(type='READY', cname='client1', state=State.ON, config=configuration)]
+	client_name = f'client{random.randint(1, 100000)}'
+	ready_request = [ReadyReq(type='READY', cname=client_name, state=State.ON, config=configuration)]
 	for rr in ready_request:
 		yield transportRequest(ready_req=rr)
 	#return transportRequest(ready_req=ReadyReq(type='READY', cname='client1', state=State.ON, config=configuration))
 
 def send_message(stub):
+	global client_name
+
+	# ready client
 	ready_info_dict = dict()
 	ready = request_ready()
 	response_ready = stub.transport(ready)
 	for rs in response_ready:
-		ready_info_dict['current_round'] = rs.ready_rep.coonfig['current_round']
-		ready_info_dict['max_train_round'] = rs.ready_rep.coonfig['max_train_round']
-		ready_info_dict['gmodel_acc'] = rs.ready_rep.coonfig['model_acc']
-		ready_info_dict['gmodel_loss'] = rs.ready_rep.coonfig['model_loss']
-		ready_info_dict['train_model_type'] = rs.ready_rep.coonfig['model_type']
-		ready_info_dict['model_version'] = rs.ready_rep.coonfig['model_version']
+		ready_info_dict['cr'] = rs.ready_rep.coonfig['current_round']
+		ready_info_dict['mtr'] = rs.ready_rep.coonfig['max_train_round']
+		ready_info_dict['ga'] = rs.ready_rep.coonfig['model_acc']
+		ready_info_dict['gl'] = rs.ready_rep.coonfig['model_loss']
+		ready_info_dict['tmt'] = rs.ready_rep.coonfig['model_type']
+		ready_info_dict['mv'] = rs.ready_rep.coonfig['model_version']
 
-	update = request_update(ready_info_dict)
+	# update client
+	update = request_parameter(ready_info_dict)
 	response_update = stub.transport(update)
 	for ru in response_update:
-		print(ru)
+		class_for_learning = learning_fit(ready_info_dict['mv'], 16, ru.update_rep.buffer_chunk, ready_info_dict['cr'])
+	print(f"### Model Training - Round: {ready_info_dict['cr']} ###")
+	get_params = class_for_learning.manage_train() # model fit
+	# update complete
+	
 
 def run():
 	channel = grpc.insecure_channel('localhost:8890')
@@ -42,4 +55,5 @@ def run():
 	send_message(stub)
 
 if __name__ == '__main__':
+	client_name = ""
 	run()
